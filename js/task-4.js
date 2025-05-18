b3fb3371a7e57c84c2824c8435e9a1554660b567
Ruslan Shevchyk <ryslansvarschik@gmail.com>
2025-05-18 20:51:52 +0200

    fix2

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ function getShippingCost(country) {
             return `Shipping to ${country} will cost 250 credits`;
         case `Australia`:
             return `Shipping to ${country} will cost 170 credits`;
-        ase `Jamaica`:
+        case `Jamaica`:
             return `Shipping to ${country} will cost 120 credits`;
         default:
             return "Sorry, there is no delivery to your country"
